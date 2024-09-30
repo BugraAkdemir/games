198e8a3dc4589b067f3d03bc5bfcf84afd617be5
Bugra <bugrakaptan5@gmail.com>
2024-09-30 19:44:51 +0300

    v.1.0

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oyun İndir</title>
+    <link rel="icon" href="../favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+        body{
+            height: 100vh;
+            display: inline-block;
+        }
+        .cart{
+            background-color: aliceblue;
+            font-size: 20px;
+            color: black;
+            width: 230px;
+            height: 170px;
+        }
+
+        .heads{
+            text-align: center;
+        }
+
+        .mtn{
+            text-align: center;
+        }
+
+        .dowBtn a{
+            color: black;
+        }
+        .dowBtn{
+            align-items: center;
+            text-align: center;
+            
+        }
+    </style>
+</head>
+<body>
+    <div class="conteiner">
+        <div class="cart">
+            <div class="heads">
+                <h3>Minecraft</h3>
+            </div>
+            <div class="mtn">
+                <p>Minecraft  1.21 İndir</p>
+            </div>
+            <div class="dowBtn">
+                <button><a href="">İndir</a></button>
+            </div>
+        </div>
+    </div>
+</html>
